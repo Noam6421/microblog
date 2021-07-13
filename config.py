49bc40629b64f0d04f522dd629f08ac7833ec754
Noam6421 <noam6421@gmail.com>
2021-07-13 12:14:27 +0300

    Chapter 9: Pagination

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['noam6421@gmail.com']
+    POSTS_PER_PAGE = 25
